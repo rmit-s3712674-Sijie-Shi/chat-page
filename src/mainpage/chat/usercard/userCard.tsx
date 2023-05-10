7179490b69f41d5095ae594c8cd56b2c94ace18b
rikusrocks <rikusrocks@gmail.com>
2023-05-10 21:20:16 +1000

    minor update

--- src/mainpage/chat/usercard/userCard.tsx
+++ src/mainpage/chat/usercard/userCard.tsx
@@ -8,6 +8,11 @@ const UserCard = () => {
                 <img className={styles.avatar} src="https://img.freepik.com/premium-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt="avatar" /> 
                 <div className={styles.user}>usercard</div>
             </div>
+
+            <div className={styles.usercard}> 
+                <img className={styles.avatar} src="https://img.freepik.com/premium-vector/man-avatar-profile-round-icon_24640-14044.jpg" alt="avatar" /> 
+                <div className={styles.user}>usercard2</div>
+            </div>
         </>
     )
 }
