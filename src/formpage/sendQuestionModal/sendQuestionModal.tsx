52e8215268a1c6604bca50f59ce9467a7aa1486b
rikusrocks <rikusrocks@gmail.com>
2023-08-28 16:29:04 +1000

    add permission input

--- src/formpage/sendQuestionModal/sendQuestionModal.tsx
+++ src/formpage/sendQuestionModal/sendQuestionModal.tsx
@@ -3,6 +3,7 @@ import styles from "./sendQuestionModal.module.css";
 import { Modal, Box, Button } from "@mui/material";
 const SendQuestionModal = ({ formID }: { formID: string }) => {
   const [open, setOpen] = React.useState(false);
+  const [permission, setPermission] = useState("every one");
   const handleSaveModalOpen = () => {
     setOpen(true);
   };
@@ -10,17 +11,37 @@ const SendQuestionModal = ({ formID }: { formID: string }) => {
     setOpen(false);
   };
 
+  const handleOptionChange = (value: any) => {
+    setPermission(value);
+  };
+
   return (
     <>
       <button onClick={handleSaveModalOpen}>Send</button>
-        <Modal open={open} onClose={handleSaveModalClose} sx={{display:"flex", justifyContent:"center", alignItems:"center"}}>
-          <Box className={styles.container}>
-            <h2 id="child-modal-title">Set Permission For this Form</h2>
-            <p id="child-modal-description">{formID}</p>
-            <button onClick={handleSaveModalClose} className={styles.cancelButton}>Close</button>
-            <button onClick={handleSaveModalClose} className={styles.sendButton}>Send</button>
-          </Box>
-        </Modal>
+      <Modal
+        open={open}
+        onClose={handleSaveModalClose}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Box className={styles.container}>
+          <h2 id="child-modal-title">Set Permission For this Form</h2>
+          <p id="child-modal-description">{formID}</p>
+          <select onChange={(e) => handleOptionChange(e.target.value)} value={permission}>
+            <option value="every one">every one</option>
+            <option value="certain people">certain people</option>
+          </select>
+          {permission === "certain people" && <input placeholder="input user email"></input>}
+          <button
+            onClick={handleSaveModalClose}
+            className={styles.cancelButton}
+          >
+            Close
+          </button>
+          <button onClick={handleSaveModalClose} className={styles.sendButton}>
+            Send
+          </button>
+        </Box>
+      </Modal>
     </>
   );
 };
